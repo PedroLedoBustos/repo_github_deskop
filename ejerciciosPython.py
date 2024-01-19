cdfdd2dae87c277f8cfd1124f53ba45a8246e029
PedroLedoBustos <152484958+PedroLedoBustos@users.noreply.github.com>
2024-01-19 17:16:12 +0000

    Final ejercicios principiante

--- ejerciciosPython.py
+++ ejerciciosPython.py
@@ -250,4 +250,11 @@ L = [3,6,9,12,15,18,21,24]
 L1= [i//3 for i in L]
 print(L1)
 
+# Ejercicio 33 #
+    # El programa para crear la variable L y asignarle la lista [-6,5,-3,-1,2,8,-3.6], luego crear una nueva lista L1 usando comprension 
+    # de lista con los numeros de L que son estrictamente mayores que 0 y finalmente mostrar la lista L1 en la consola.
+L = [-6,5,-3,-1,2,8,-3.6]
+L1= [i for i in L if i>0]
+print(L1)
+
 
